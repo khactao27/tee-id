0fe1297eb6d4b9f9584e43e3623f51653b23b30a
tao.duongkhac <tao.duongkhac@gmail.com>
2023-03-13 06:56:25 +0700

    add store key

--- src/scenes/Splash/index.js
+++ src/scenes/Splash/index.js
@@ -1,50 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { Text, ActivityIndicator, Image } from "react-native";
 import { SafeAreaView } from "react-native";
 import styles from "./styles";
 import images from '@assets/images'
 import { connect } from 'react-redux'
 import { loadUser } from './actions'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export class Splash extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            animating: true,
-        };
-      }
-
-    componentDidMount() {
-        console.log('splash', this.props.splasher)
-        // setTimeout(() => {
-        //     this.props.navigation.replace('Auth')
-        // }, 3000)
-    }
-
-    render() {
-        return (
-            <SafeAreaView style={styles.container}>
-                {console.log(this.props.splasher)}
-                <Image source={images.logo}
-                    style={{ width: '90%', resizeMode: 'contain', margin: 30 }} />
-                <ActivityIndicator
-                    animating={this.state.animating}
-                    color="#FFFFFF"
-                    size="large"
-                    style={styles.activityIndicator}
-                />
-            </SafeAreaView>
-        )
+const storeData = async (key, value) => {
+    try {
+      await AsyncStorage.setItem(key, value)
+    } catch (e) {
+      // saving error
     }
 }
 
-const mapStateToProps = state => ({
-    splasher: state.splash
-});
+const getData = async (key) => {
+    try {
+      return await AsyncStorage.getItem(key)
+    } catch(e) {
+      return 
+    }
+}
+
+
+export const Splash = ({ navigation }) => {
+
+    const [animating, setAnimating] = useState(true)
+
+    useEffect(async () => {
+        console.log('prive', await getData('privateKey'))
+    })
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image source={images.logo}
+                style={{ width: '90%', resizeMode: 'contain', margin: 30 }} />
+            <ActivityIndicator
+                animating={animating}
+                color="#FFFFFF"
+                size="large"
+                style={styles.activityIndicator}
+            />
+        </SafeAreaView>
+    )
+}
 
 export default connect(
-    mapStateToProps,
+    state => ({
+        splash: state.splash
+    }),
     dispatch => ({
         load: (appStore) => dispatch(loadUser(appStore))
     })
